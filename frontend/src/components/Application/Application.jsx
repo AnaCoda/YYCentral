e288aa300d9dcbc5a6a7013771aa48d0c57cc9c9
Nickil13 <36387179+Nickil13@users.noreply.github.com>
2022-07-23 16:18:10 -0600

    Basic event structure

--- frontend/src/components/Application/Application.jsx
+++ frontend/src/components/Application/Application.jsx
@@ -1,13 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+
 import { DisplayMapComponent } from "../../DisplayMapComponent";
+import Events from "../../views/Events";
 import NavBar from "../NavBar/NavBar";
-import React from 'react';
+import React from "react";
 import styles from "./Application.module.scss";
 
 export default function Application() {
-  return (
-    <div className={styles.Application}>
-        <NavBar/>
-        <DisplayMapComponent/>
-    </div>
-  )
+    return (
+        <div className={styles.Application}>
+            <NavBar />
+            <Switch>
+                <Route
+                    path="/app"
+                    exact
+                    component={DisplayMapComponent}
+                    index
+                />
+                <Route path="/app/events" component={Events} />
+            </Switch>
+        </div>
+    );
 }
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,32 +1,38 @@
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import { IconButton } from "@mui/material";
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import LogoutIcon from '@mui/icons-material/Logout';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-import React from 'react';
+import { Link } from "react-router-dom";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LogoutIcon from "@mui/icons-material/Logout";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import React from "react";
 import styles from "./NavBar.module.scss";
 
 export default function NavBar() {
-   return (
-      <div className={styles.navbar__wrapper}>
-         <div className={styles.navbar}>
-            <div className={styles.navbar__item}>
-               <IconButton className={styles.icon} onClick={() => console.log("test")}>
-                  <QuestionAnswerIcon/>
-               </IconButton>
-               <IconButton className={styles.icon}>
-                  <CalendarTodayIcon/>
-               </IconButton>
-               <IconButton className={styles.icon}>
-                  <LocationOnIcon/>
-               </IconButton>
+    return (
+        <div className={styles.navbar__wrapper}>
+            <div className={styles.navbar}>
+                <div className={styles.navbar__item}>
+                    <IconButton
+                        className={styles.icon}
+                        onClick={() => console.log("test")}
+                    >
+                        <QuestionAnswerIcon />
+                    </IconButton>
+                    <IconButton className={styles.icon}>
+                        <Link to="/app/events">
+                            <CalendarTodayIcon />
+                        </Link>
+                    </IconButton>
+                    <IconButton className={styles.icon}>
+                        <LocationOnIcon />
+                    </IconButton>
+                </div>
+                <div className={styles.navbar__bottom}>
+                    <IconButton className={styles.icon}>
+                        <LogoutIcon />
+                    </IconButton>
+                </div>
             </div>
-            <div className={styles.navbar__bottom}>
-               <IconButton className={styles.icon}>
-                  <LogoutIcon/>
-               </IconButton>
-            </div>
-         </div>
-      </div>
-   )
+        </div>
+    );
 }
