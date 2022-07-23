3175bc32896cde983bc661de404a0bb467d17a36
Nickil13 <36387179+Nickil13@users.noreply.github.com>
2022-07-23 09:27:08 -0600

    [YYC-5] Added basic styling system and header structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,75 @@
-import './App.css';
+import "./App.css";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-   Redirect,
-   Route,
-   BrowserRouter as Router,
-   Switch
+    Redirect,
+    Route,
+    BrowserRouter as Router,
+    Switch,
 } from "react-router-dom";
 
-import Navbar from "./components/NavBar/NavBar";
-import SignIn from './LoginView';
-import ToDoView from './ToDoView';
+import SignIn from "./LoginView";
+import ToDoView from "./ToDoView";
+import LandingPage from "./views/LandingPage";
 
 function App() {
-   return (
-      <Router>
-         <AppEntry />
-      </Router>
-   )
+    return (
+        <Router>
+            <AppEntry />
+        </Router>
+    );
 }
 
 function AppEntry() {
-   const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user') || '{}'));
+    const [user, setUser] = useState(
+        JSON.parse(sessionStorage.getItem("user") || "{}")
+    );
 
-   function updateUserInfo(userDetail) {
-      setUser(userDetail);
-      sessionStorage.setItem("user", JSON.stringify(userDetail));
-      sessionStorage.setItem('loggedin', true);
-   }
+    function updateUserInfo(userDetail) {
+        setUser(userDetail);
+        sessionStorage.setItem("user", JSON.stringify(userDetail));
+        sessionStorage.setItem("loggedin", true);
+    }
 
-   // A wrapper for <Route> that redirects to the login
-   // screen if you're not yet authenticated.
-   function PrivateRoute({ children, ...rest }) {
-      return (
-         <Route
-            {...rest}
-            render={({ location }) =>
-               (sessionStorage.getItem('loggedin') != null && sessionStorage.getItem('loggedin')) ? (
-                  children
-               ) : (
-                     <Redirect
-                        to={{
-                           pathname: "/login",
-                           state: { from: location }
-                        }}
-                     />
-                  )
-            }
-         />
-      );
-   }
+    // A wrapper for <Route> that redirects to the login
+    // screen if you're not yet authenticated.
+    function PrivateRoute({ children, ...rest }) {
+        return (
+            <Route
+                {...rest}
+                render={({ location }) =>
+                    sessionStorage.getItem("loggedin") != null &&
+                    sessionStorage.getItem("loggedin") ? (
+                        children
+                    ) : (
+                        <Redirect
+                            to={{
+                                pathname: "/login",
+                                state: { from: location },
+                            }}
+                        />
+                    )
+                }
+            />
+        );
+    }
 
-   return (
-      <section>
-         <Switch>
-            <Route path="/login">
-               {/* 
+    return (
+        <section>
+            <Switch>
+                <Route path="/" component={LandingPage} exact />
+                <Route path="/login">
+                    {/* 
                Sign In control - Feel free to customize
                 */}
-               <SignIn updateUserInfo={updateUserInfo} />
-            </Route>
-            {/* <PrivateRoute path="*">
-               
-               <ToDoView userdetails={user} />
-            </PrivateRoute> */}
-            <Route path="/nav" component={Navbar} />
-         </Switch>
-      </section>
-   );
+                    <SignIn updateUserInfo={updateUserInfo} />
+                </Route>
+                <PrivateRoute path="*">
+                    <ToDoView userdetails={user} />
+                </PrivateRoute>
+            </Switch>
+        </section>
+    );
 }
 
 export default App;
