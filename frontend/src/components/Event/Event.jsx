596adbf8c1ff85d3b92a253f972fd6d111266afa
natewu <36091727+natewu@users.noreply.github.com>
2022-07-23 18:22:03 -0600

    [yyc-6] Event card UI, navbar remove state when exit map, jsconfig

--- frontend/src/components/Event/Event.jsx
+++ frontend/src/components/Event/Event.jsx
@@ -1,5 +1,8 @@
+import CloseIcon from '@mui/icons-material/Close';
 import { IconButton } from "@mui/material";
 import React from 'react';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import StarIcon from '@mui/icons-material/Star';
 import { setEvent } from "../../redux/reducers/appSlice";
 import styles from './Event.module.scss';
 import { useDispatch } from "react-redux";
@@ -14,27 +17,51 @@ export default function Event({event}) {
 
    return (
       <div className={styles.event}>
-         <div className={styles.title}>
-            {event.title}
-            <IconButton onClick={exit}>
-               exit
+         <div className={styles.header}>
+            <h1 className={styles.header__title}>
+               Event
+            </h1>
+            <IconButton className={styles.header__exit} onClick={exit}>
+               <CloseIcon sx={{color: "white"}}/>
             </IconButton>
          </div>
-         <div className={styles.details}>
-            <div className={styles.rating}>
-               5 star rating :O
+         <div className={styles.body}>
+            <div className={styles.title}>
+               <h1>
+                  {event.title}
+               </h1>
+               <ShoppingBagIcon sx={{flex: 0.1, margin: "1.25rem", fontSize:"2rem", color: "rgb(255, 196, 0)"}}/>
             </div>
-            <div className={styles.tags}>
-               tech
+            <div className={styles.details}>
+               <div className={styles.rating}>
+                  <StarIcon className={styles.rating}/>
+                  <StarIcon className={styles.rating}/>
+                  <StarIcon className={styles.rating}/>
+                  <StarIcon className={styles.rating}/>
+                  <StarIcon className={styles.rating}/>
+               </div>
+               <div className={styles.tags}>
+                  <h1 className={styles.details__header}>
+                     Tags
+                  </h1>
+                  <div className={styles.details__content}>
+                     Tech, Hackathon, Food
+                  </div>
+               </div>
+               <div className={styles.location}>
+                  <h1 className={styles.details__header}>
+                     Location
+                  </h1>
+                  <div className={styles.details__content}>
+                     {event.location}
+                  </div>
+               </div>
+               <div className={styles.phone}>
+                  {event.phone}
+               </div>
             </div>
-            <div className={styles.location}>
-               {event.location}
-            </div>
-            <div className={styles.phone}>
-               {event.phone}
-            </div>
+            <div className={styles.description} dangerouslySetInnerHTML={{__html: event.description}} />
          </div>
-         <div className={styles.description} dangerouslySetInnerHTML={{__html: event.description}} />
          
       </div>
    )
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,43 @@
+import { Link, useHistory } from "react-router-dom";
+
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import { IconButton } from "@mui/material";
-import { Link } from "react-router-dom";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import LogoutIcon from "@mui/icons-material/Logout";
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 import React from "react";
+import { setEvent } from "../../redux/reducers/appSlice";
 import styles from "./NavBar.module.scss";
+import { useDispatch } from "react-redux";
 
 export default function NavBar() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    function push(path) {
+        history.push(path);
+        
+        // Reset all redux states
+        dispatch(
+            setEvent(undefined)
+        );
+    }
+
     return (
         <div className={styles.navbar__wrapper}>
             <div className={styles.navbar}>
                 <div className={styles.navbar__item}>
                     <IconButton
                         className={styles.icon}
-                        onClick={() => console.log("test")}
                     >
                         <QuestionAnswerIcon />
                     </IconButton>
-                    <IconButton className={styles.icon}>
+                    <IconButton className={styles.icon} onClick={() => push("/app/events")}>
                         <Link to="/app/events">
                             <CalendarTodayIcon />
                         </Link>
                     </IconButton>
-                    <IconButton className={styles.icon}>
+                    <IconButton className={styles.icon} onClick={() => push("/app")}>
                         <LocationOnIcon />
                     </IconButton>
                 </div>
